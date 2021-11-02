ddb2a5ea3520fab7cc5ea0751919e9a80f024a8a
Umama Khalid <umamakhalid39@gmail.com>
2021-11-02 15:06:48 +0500

    Update readingDB.java

--- readingDB.java
+++ readingDB.java
@@ -4,7 +4,7 @@ public void readingDB(){
         Cursor cursorRecordings;
         SQLiteDatabase sqLiteDatabase;
 
-        sqLiteDatabase = SQLiteDatabase.openDatabase(Environment.getExternalStorageDirectory().getAbsolutePath() + "/uStethApp/databases/24-08-2021_1056_database.db",
+        sqLiteDatabase = SQLiteDatabase.openDatabase(Environment.getExternalStorageDirectory().getAbsolutePath() + "/foo/databases/24-08-2021_1056_database.db",
                 null, SQLiteDatabase.OPEN_READONLY
                         | SQLiteDatabase.NO_LOCALIZED_COLLATORS
                         | SQLiteDatabase.CREATE_IF_NECESSARY);
@@ -21,7 +21,7 @@ public void readingDB(){
             do {
 
                 Log.e(TAG, "audio_ID: "+
-                        cursorRecordings.getString(0) + ", Patient_Id:  " +
+                        cursorRecordings.getString(0) + ", Id:  " +
                         cursorRecordings.getString(1) + ", server pat id:  " +
                         cursorRecordings.getString(7) + ", server_id: " +
                         cursorRecordings.getString(11)
